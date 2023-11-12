49debd95f2b0edc679a2f8d8c7f21d9ad70a05b3
TUJungMin <paulkjm92@gmail.com>
2023-11-12 21:07:23 +0900

    맥주 랜덤 생성 좌우 생성 완료

--- beer.py
+++ beer.py
@@ -1,13 +1,35 @@
-
 from pico2d import *
-
+from math import radians, sin, cos, sqrt
+import random
 
 target_path = 'beer.png'  # 맥주병 이미지 파일 경로
+
 class Beer:
-    def __init__(self, x, y):
+    def __init__(self, x, y, direction):
         self.x = x
         self.y = y
         self.image = load_image(target_path)
+        self.angle = 0  # 초기 회전 각도
+        self.time = 0
+        self.direction = direction
+
+        if self.direction == 1:
+            self.initial_velocity = 1  # 초기 속도 (왼쪽에서 오른쪽으로)
+        else:
+            self.initial_velocity = -1  # 초기 속도 (오른쪽에서 왼쪽으로)
 
     def draw(self):
-        self.image.draw(self.x, self.y, 200, 100)
+        self.image.rotate_draw(radians(self.angle), self.x, self.y, 300, 100)
+
+    def update(self):
+        gravity = 0  # 중력 가속도를 0으로 설정하여 직선 운동으로 변경
+        angle_radians = radians(self.angle)
+
+        self.x += self.initial_velocity * cos(angle_radians) * self.time
+        self.y += self.initial_velocity * sin(angle_radians) * self.time - gravity * (self.time ** 2)
+
+        self.time += 0.1  # 시간 증가
+
+    def is_clicked(self, mx, my):
+        distance = sqrt((self.x - mx) ** 2 + (self.y - my) ** 2)
+        return distance <= 100
--- project.py
+++ project.py
@@ -6,11 +6,10 @@ WIDTH, HEIGHT = 1200, 700  # 화면 크기
 background_path = 'map.jpg'  # 배경 사진 파일 경로
 cursor_path = 'scope.png'  # 마우스 커서 이미지 파일 경로
 
-
 x_pos, y_pos = 0, 0
 beers = []  # 맥주병 객체들을 저장할 리스트
 beer_timer = 0
-beer_interval = 2  # 2초마다 맥주 생성
+beer_interval = 1  # 1초마다 맥주 생성
 
 def reset_world():
     global background, cursor
@@ -27,11 +26,25 @@ def render_world(mx, my):
 
     update_canvas()
 
+def update_world():
+    global beers
+    for beer in beers:
+        beer.update()
+
+def remove_clicked_beers(mx, my):
+    global beers
+    clicked_beers = [beer for beer in beers if beer.is_clicked(mx, my)]
+
+    for clicked_beer in clicked_beers:
+        beers.remove(clicked_beer)
 def generate_beer():
     global beer_timer
-    beers.append(Beer(random.randint(0, WIDTH), random.randint(0, HEIGHT)))
+    direction = random.choice([1, 2])
+    if direction == 1:
+        beers.append(Beer(-100, random.randint(400, HEIGHT), direction))
+    else:
+        beers.append(Beer(WIDTH + 100, random.randint(400, HEIGHT), direction))
     beer_timer = get_time()
-
 def get_mouse_pos():
     global x_pos, y_pos
     x, y = x_pos, y_pos
@@ -58,12 +71,17 @@ while running:
     if get_time() - beer_timer > beer_interval:
         generate_beer()
 
-    delay(0.01)
+    update_world()
 
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
+        elif event.type == SDL_MOUSEBUTTONDOWN and event.button == SDL_BUTTON_LEFT:
+            # 왼쪽 마우스 버튼 클릭 시 맥주병 제거
+            remove_clicked_beers(mx, my)
+
+    delay(0.01)
 
 # finalization code
 close_canvas()
