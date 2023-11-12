6df63e83b108c520e473b5d7d716c0ce4c85367f
TUJungMin <paulkjm92@gmail.com>
2023-11-12 18:34:05 +0900

    스코프 이미지 다운

--- project.py
+++ project.py
@@ -1,53 +1,45 @@
 from pico2d import *
-from map import Map
-WIDTH, HEIGHT = 1200, 600  # 원하는 화면 크기
-# Game object class here
 
-def handle_events():
-    global running
+WIDTH, HEIGHT = 1200, 700  # 화면 크기
+background_path = 'map.jpg'  # 배경 사진 파일 경로
+cursor_path = 'scope.png'  # 마우스 커서 이미지 파일 경로
+
+def reset_world():
+    global background, cursor
+    background = load_image(background_path)
+    cursor = load_image(cursor_path)
+
+def render_world(mx, my):
+    clear_canvas()
+    background.draw(WIDTH // 2, HEIGHT // 2, WIDTH, HEIGHT)
+    cursor.draw(mx, my, 50, 50)  # 마우스 위치에 따라 커서 그리기
+    update_canvas()
+def get_mouse_pos():
+    x, y = 0, 0
+    events = get_events()
+    for event in events:
+        if event.type == SDL_MOUSEMOTION:
+            x, y = event.x, HEIGHT - event.y-1  # Y 좌표는 화면의 반전된 값으로 처리
+    return x, y
+
+
+open_canvas(WIDTH, HEIGHT)
+reset_world()
+
+# game loop
+running = True
+
+
+
+
+while running:
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-            running = False
-        else:
-            # 'boy.handle_event(event)' 대신에 적절한 처리 로직 추가
-            pass
 
-def reset_world():
-    global running, world, boy
-
-    running = True
-    world = []
-
-    open_canvas(WIDTH, HEIGHT)  # 화면 크기 설정
-
-    background = Map()
-    world.append(background)
-
-    # 추가적인 game object를 생성하고 world에 추가
-    # 예: boy = Boy()와 같은 코드를 이곳에 추가
-    # world.append(boy)
-
-def update_world():
-    for o in world:
-        o.update()
-
-def render_world():
-    clear_canvas()
-    for o in world:
-        o.draw()
-    update_canvas()
-
-open_canvas()
-reset_world()
-
-# game loop
-while running:
-    handle_events()
-    update_world()
-    render_world()
+    mx, my = get_mouse_pos()  # 마우스의 현재 위치 얻기
+    render_world(mx, my)
     delay(0.01)
 
 # finalization code
